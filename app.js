9aa0336b1066994f7dc861d3bc5d9e4a2d882160
Airu-Ok <gutierrez.charcas97@gmail.com>
2021-11-20 01:32:34 -0300

    Mayra subio login y register

--- app.js
+++ app.js
@@ -9,3 +9,10 @@ app.listen(3010 , ()=>{
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/index.html');
 });
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
